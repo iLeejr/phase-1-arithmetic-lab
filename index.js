69254a9cca79b4489f1919525ce5ef0152c1138d
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-17 13:37:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-
 const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
 
-i cannot figure this one out :/
+const random = Math.floor(Math.random() * 10) + 1
 
 const num3 = 10;
 const num4 = 6;
 const mod = num3 % num4;
 
-const max = 20;
 
-module.exports = {
-  num1,
-  num2,
-  multiply,
-  random,
-  num3,
-  num4,
-  mod,
-  max
-};
+let max = Math.max(1, 5, 10, 20)
